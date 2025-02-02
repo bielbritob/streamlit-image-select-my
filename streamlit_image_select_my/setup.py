2d96dedbbb1aa1846d8c9457d6ff38f5ba65be80
bielbritob <123404262+bielbritob@users.noreply.github.com>
2025-02-02 19:09:31 -0400

    Add files via upload

---
+++ streamlit_image_select_my/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="streamlit-image-select-my",  # Nome ÚNICO para evitar conflitos!
+    version="1.0.0",  # Altere conforme sua versão
+    packages=find_packages(),
+    install_requires=["streamlit"],  # Adicione outras dependências se necessário
+)
